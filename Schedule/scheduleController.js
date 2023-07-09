7e7e361e0c3add3bd9b8af51c6e62a09e79c1ef5
oxotnik44 <artemnovikov1969@gmail.com>
2023-07-09 19:58:11 +0700

    добавил четность не четность недели в расписании

--- Schedule/scheduleController.js
+++ Schedule/scheduleController.js
@@ -2,19 +2,19 @@
 const db = require("../db");
 const moment = require("moment");
 
-function isWeekEven() {
-  const currentDate = new Date();
-  const firstDayOfYear = new Date(currentDate.getFullYear(), 0, 1);
-  const pastDaysOfYear = (currentDate - firstDayOfYear) / 86400000;
-  const weekNumber = Math.ceil(
-    (pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7
-  );
-  return weekNumber % 2 === 0;
+function getWeekNumber() {
+  const currentDate = moment();
+  const startOfYear = moment().startOf("year");
+  const weekNumber = currentDate.diff(startOfYear, "weeks") + 1;
+  return weekNumber;
 }
 
 exports.getSchedule = (req, res) => {
   const { id_group } = req.body;
-  const isEvenWeek = isWeekEven();
+  const currentWeek = getWeekNumber();
+  const isEvenWeek = currentWeek % 2 === 0;
+  const nextWeek = currentWeek === 4 ? 1 : currentWeek + 1;
+
 
   db.query(
     `SELECT
@@ -44,7 +44,8 @@ exports.getSchedule = (req, res) => {
       ) AS nameEducator,
       dek_group_predmet.id_prep AS idEducator,
       dek_prepod.regalia AS regaliaEducator,
-      NULL AS date
+      NULL AS date,
+      ${currentWeek} AS weekNumber
     FROM dek_group_predmet
     LEFT JOIN dek_prepod ON dek_prepod.id = dek_group_predmet.id_prep
     LEFT JOIN dek_room ON dek_room.id = dek_group_predmet.id_room
@@ -52,41 +53,38 @@ exports.getSchedule = (req, res) => {
     WHERE dek_group_predmet.id_group = ${id_group}
       AND dek_group_predmet.id_prep != -1
       AND dek_group_predmet.chetnechet IN (1, 2)
-    
   
     UNION
   
     SELECT
-  dek_zgroup_predmet.id AS idPair,
-  dek_room.number AS roomNumber,
-  NULL AS chetnechet,
-  '' AS weekday,
-  dek_zgroup_predmet.para AS numberPair,
-  dek_cpoints.short AS typePair,
-  dek_zgroup_predmet.predmet AS namePair,
-  CONCAT(
-    SUBSTRING_INDEX(dek_prepod.name, ' ', 1),
-    ' ',
-    UPPER(SUBSTRING(SUBSTRING_INDEX(dek_prepod.name, ' ', -1), 1, 1)),
-    '.',
-    UPPER(SUBSTRING(SUBSTRING_INDEX(dek_prepod.name, ' ', -1), 2, 1)),
-    '.'
-  ) AS nameEducator,
-  dek_zgroup_predmet.id_prep AS idEducator,
-  dek_prepod.regalia AS regaliaEducator,
-  dek_zgroup_predmet.date AS date
-FROM dek_zgroup_predmet
-LEFT JOIN dek_prepod ON dek_prepod.id = dek_zgroup_predmet.id_prep
-LEFT JOIN dek_room ON dek_room.id = dek_zgroup_predmet.id_room
-LEFT JOIN dek_cpoints ON dek_cpoints.id = dek_zgroup_predmet.zach_exam
-WHERE dek_zgroup_predmet.id_group = ${id_group}
-  AND dek_zgroup_predmet.id_prep != -1
-  AND dek_zgroup_predmet.date >= CURDATE()
-  AND dek_zgroup_predmet.date < CURDATE() + INTERVAL 1 WEEK
-ORDER BY date ASC, numberPair ASC
-
-  
-`,
+      dek_zgroup_predmet.id AS idPair,
+      dek_room.number AS roomNumber,
+      NULL AS chetnechet,
+      '' AS weekday,
+      dek_zgroup_predmet.para AS numberPair,
+      dek_cpoints.short AS typePair,
+      dek_zgroup_predmet.predmet AS namePair,
+      CONCAT(
+        SUBSTRING_INDEX(dek_prepod.name, ' ', 1),
+        ' ',
+        UPPER(SUBSTRING(SUBSTRING_INDEX(dek_prepod.name, ' ', -1), 1, 1)),
+        '.',
+        UPPER(SUBSTRING(SUBSTRING_INDEX(dek_prepod.name, ' ', -1), 2, 1)),
+        '.'
+      ) AS nameEducator,
+      dek_zgroup_predmet.id_prep AS idEducator,
+      dek_prepod.regalia AS regaliaEducator,
+      dek_zgroup_predmet.date AS date,
+      ${nextWeek} AS weekNumber
+    FROM dek_zgroup_predmet
+    LEFT JOIN dek_prepod ON dek_prepod.id = dek_zgroup_predmet.id_prep
+    LEFT JOIN dek_room ON dek_room.id = dek_zgroup_predmet.id_room
+    LEFT JOIN dek_cpoints ON dek_cpoints.id = dek_zgroup_predmet.zach_exam
+    WHERE dek_zgroup_predmet.id_group = ${id_group}
+      AND dek_zgroup_predmet.id_prep != -1
+      AND dek_zgroup_predmet.date >= CURDATE()
+      AND dek_zgroup_predmet.date < CURDATE() + INTERVAL 1 WEEK
+    ORDER BY date ASC, numberPair ASC`,
     (error, rows) => {
       const timeIntervals = [
         "8:30-10:00",
@@ -99,17 +97,19 @@ ORDER BY date ASC, numberPair ASC
       ];
       if (error) {
         console.log(error);
-        res.status(500).json({ error: "An error occurred" });
+        res.status(500).json({ error: "Произошла ошибка" });
       } else {
         const filteredRows = rows.filter((row) => {
-          // Фильтрация записей на основе четности недели
-          if (row.chetnechet === 1 && isEvenWeek) {
+          if (row.chetnechet === 1 && row.weekNumber === currentWeek) {
             return true;
           }
-          if (row.chetnechet === 2 && !isEvenWeek) {
+          if (row.chetnechet === 2 && row.weekNumber === currentWeek) {
             return true;
           }
-          if (row.chetnechet === null) {
+          if (row.chetnechet === 1 && row.weekNumber === nextWeek) {
+            return true;
+          }
+          if (row.chetnechet === 2 && row.weekNumber === nextWeek) {
             return true;
           }
           return false;
@@ -126,7 +126,28 @@ ORDER BY date ASC, numberPair ASC
           }
         });
 
-        res.status(200).json(filteredRows);
+        const schedule = {
+          even: [],
+          odd: [],
+        };
+
+        filteredRows.forEach((row) => {
+          if (row.chetnechet === 1 && row.weekNumber === currentWeek) {
+            schedule.even.push(row);
+          } else if (row.chetnechet === 2 && row.weekNumber === currentWeek) {
+            schedule.odd.push(row);
+          } else if (row.chetnechet === 1 && row.weekNumber === nextWeek) {
+            schedule.even.push(row);
+          } else if (row.chetnechet === 2 && row.weekNumber === nextWeek) {
+            schedule.odd.push(row);
+          }
+        });
+  
+        const result = {
+          schedule,
+        };
+
+        res.status(200).json(result);
       }
     }
   );
